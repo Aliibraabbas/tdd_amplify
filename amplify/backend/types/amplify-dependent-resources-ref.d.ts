7708c16638bb555bd8da7603f3e5474d40be74a0
Ali ABBAS <ali.ibra.abbas@gmail.com>
2025-07-06 21:31:42 +0200

    feat add api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "userApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "function": {
     "userHandler": {
       "Arn": "string",
